896cd9ce59dc6c62490ceca5392955ca66a3d78a
michalswi <s_z_p@o2.pl>
2018-06-15 13:23:18 +0200

    redesign+open SB

---
+++ open-service-broker/README.md
@@ -0,0 +1,27 @@
+### Test Open Service Broker
+
+Open Broker API repo is [here](https://github.com/eruvanos/openbrokerapi).  
+
+OB API python code is [here](https://pypi.org/project/openbrokerapi/).  
+
+OB API details are [here](http://openbrokerapi.readthedocs.io/en/latest/openbrokerapi.html).  
+
+Templates are [here](./templates).  
+
+Run deployment:  
+```sh
+$ kubectl create namespace testbroker
+$ kubectl create -f templates/testBroker.yaml
+```
+
+Local tests, more details [here](https://github.com/openservicebrokerapi/servicebroker/blob/v2.12/spec.md#request):  
+```sh
+$ docker run -d -p 8080:5000 local/run-broker:v0.1.0
+$ curl -v -H "X-Broker-API-Version: 2.12" http://user:pass@localhost:8080/v2/catalog
+...
+HTTP/1.0 412 PRECONDITION FAILED
+$ curl -v -H "X-Broker-API-Version: 2.13" http://user:pass@localhost:8080/v2/catalog
+...
+{"services":[{"bindable":true,"description":"Example Service does nothing"
+```
+More about Headers and API version [here](https://github.com/openservicebrokerapi/servicebroker/blob/v2.12/spec.md#api-version-header).
---
+++ open-service-broker/run_broker.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+# https://pypi.org/project/openbrokerapi/
+# http://openbrokerapi.readthedocs.io/en/latest/openbrokerapi.html
+
+from flask import Flask
+from openbrokerapi import api
+from openbrokerapi.catalog import (ServicePlan,)
+from openbrokerapi.log_util import basic_config
+from openbrokerapi.service_broker import (
+    ServiceBroker,
+    Service,
+    ProvisionedServiceSpec,
+    UpdateServiceSpec,
+    Binding,
+    DeprovisionServiceSpec,
+    LastOperation,
+    UnbindDetails,
+    ProvisionDetails,
+    UpdateDetails,
+    BindDetails,
+    DeprovisionDetails
+)
+
+class ExampleServiceBroker(ServiceBroker):
+    def catalog(self):
+        return Service(
+            id='00000000-0000-0000-0000-000000000000',
+            name='example-service',
+            description='Example Service does nothing',
+            bindable=True,
+            plans=[
+                ServicePlan(
+                    id='00000000-0000-0000-0000-000000000000',
+                    name='small',
+                    description='example service plan',
+                ),
+            ],
+            tags=['example', 'service'],
+            plan_updateable=True,
+        )
+
+    def provision(self, instance_id: str, service_details: ProvisionDetails,
+                  async_allowed: bool) -> ProvisionedServiceSpec:
+        pass
+
+    def unbind(self, instance_id: str, binding_id: str, details: UnbindDetails):
+        pass
+
+    def update(self, instance_id: str, details: UpdateDetails, async_allowed: bool) -> UpdateServiceSpec:
+        pass
+
+    def bind(self, instance_id: str, binding_id: str, details: BindDetails) -> Binding:
+        pass
+
+    def deprovision(self, instance_id: str, details: DeprovisionDetails, async_allowed: bool) -> DeprovisionServiceSpec:
+        pass
+
+    def last_operation(self, instance_id: str, operation_data: str) -> LastOperation:
+        pass
+
+
+# Simply start the server
+# api.serve(ExampleServiceBroker(), api.BrokerCredentials("", ""))
+
+# or start the server without authentication
+# api.serve(ExampleServiceBroker(), None)
+
+# or with multiple service brokers
+# api.serve([ExampleServiceBroker(), ExampleServiceBroker()], api.BrokerCredentials("", ""))
+
+# or register blueprint to your own FlaskApp instance
+app = Flask(__name__)
+logger = basic_config()  # Use root logger with a basic configuration provided by openbrokerapi.log_utils
+openbroker_bp = api.get_blueprint(ExampleServiceBroker(), api.BrokerCredentials("user", "pass"), logger)
+app.register_blueprint(openbroker_bp)
+
+#default, port 5000
+app.run("0.0.0.0")
+
+# if __name__ == "__main__":
+#     app.run("0.0.0.0", 5000, True)
+
